052bf6cfe3bb4bc6661f718ace040d81b8189280
Agus Tri Santoso <agussss.ts@gmail.com>
2021-10-23 10:24:23 +0700

    nambah list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro sed, exercitationem itaque nulla impedit tenetur aliquid minima harum voluptatibus! Vitae assumenda ex distinctio eius! Sed provident tempore obcaecati labore voluptatibus.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
